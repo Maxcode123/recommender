7de73b743b07ba3b0bd3171189fb7f135c0f49f5
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-01-27 09:46:51 +0200

    lexdriver

--- src/scanner/lexdriver.c
+++ src/scanner/lexdriver.c
@@ -7,28 +7,7 @@ YYSTYPE yylval;
 
 extern FILE *yyin;
 
-int pos = 0;
-
 int yylex(void); /* prototype for the lexing function */
-
-static char *toknames[] = {
-    "USERNAME",
-    "RATING",
-    "TITLE",
-    "DATE",
-};
-
-char *getname(int i) {
-    return i<257 || i>269 ? "BAD TOKEN" : toknames[i-257];
-}
-
-void reset(char *fname) {
-    yyin = fopen(fname, "r");
-    if (!yyin) {
-        fprintf(stderr, "cannot open file\n");
-        exit(1);
-    }
-}
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -42,8 +21,14 @@ int main(int argc, char **argv) {
 
     fname = argv[1];
     fname2 = argv[2];
-    write = fopen(fname2, "w");
-    reset(fname);
+    if (!(yyin = fopen(fname, "r"))) {
+        fprintf(stderr, "cannot open read file\n");
+        exit(1);
+    }
+    if (!(write = fopen(fname2, "w"))) {
+        fprintf(stderr, "cannot open write file\n");
+        exit(1);
+    }
 
     for (;;) {
         i = yylex();
