06ce8e172a8187c4f509887ed71ca0868f84e79b
My Name <konstantinoskolios@windowslive.com>
2023-02-18 16:45:50 +0200

    added new linked-list

--- src/linked-list/linkedlist.c
+++ src/linked-list/linkedlist.c
@@ -1,101 +1,20 @@
-#include "linkedlist.h"
-#include "stdio.h"
+#pragma  once
 
-typedef struct Node {
-    int value;
-    _Node *next;
-} _Node;
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void print_all_linkedlist(_Node *head) {
-    _Node *temp = head;
-    if (temp == NULL) {
-        printf("Empty list!\n");
-    }
-    printf("--------------\n");
-    int p = 0;
-    while (temp != NULL) {
-        printf(".%d) %d\n", p++, temp->value);
-        temp = (_Node *) temp->next;
-    }
-    printf("--------------\n");
-}
-
-void add_linkedlist(struct Node **head, int new_value) {
-    _Node *newNode = malloc(sizeof(_Node));
-    newNode->value = new_value;
-    newNode->next = *head;
-    *head = newNode;
-}
-
-int search_by_value_linkedlist(_Node *head, int value) {
-    _Node *current = head;
-    int counter = 0;
-    while (head != NULL) {
-        if (current->value == value) {
-            return counter;
-        }
-        current = (_Node *) current->next;
-        counter++;
-    }
-    return -1;
-}
-
-void sort_elements_linkedlist(_Node *head) {
-    int swapped, i;
-    _Node *node1, *node2 = NULL;
-
-    if (head == NULL) {
-        return;
-    }
-
-    do {
-        swapped = 0;
-        node1 = head;
-
-        while (node1->next != node2) {
-            if (node1->value > node1->next->value) {
-                private_swap_linkedlist(node1, node1->next);
-                swapped = 1;
-            }
-            node1 = node1->next;
-        }
-        node2 = node1;
-    } while (swapped);
-}
-
-void private_swap_linkedlist(_Node *node1, _Node *node2) {
-    int temp = node1->value;
-    node1->value = node2->value;
-    node2->value = temp;
-}
-
-int get_size_linkedlist(_Node *head) {
-    int count = 0;
-    _Node *temp = head;
-    while (temp != NULL) {
-        count++;
-        temp = temp->next;
-    }
-    return count;
-}
-
-void delete_by_value_linkedlist(_Node **head, int key) {
-    _Node *temp = *head, *prev;
-
-    if (temp != NULL && temp->value == key) {
-        *head = temp->next;
-        free(temp);
-        return;
-    }
-
-    while (temp != NULL && temp->value != key) {
-        prev = temp;
-        temp = temp->next;
-    }
-
-    if (temp == NULL) return;
-
-    prev->next = temp->next;
-
-    free(temp);
-}
+typedef struct _Node {
+    void *value;
+    struct _Node *next;
+} *Node;
+/* Initialize a new LinkedList*/
+Node ll_init(void *value);
+/* Adds a new value to the LinkedList */
+void ll_add(Node head, void *value);
+/* Removes an existing value to the LinkedList */
+void ll_remove(Node* head, void* value);
+/* Print all the values of the LinkedList by type*/
+void ll_print_all_type(Node head,char type);
+/*Gets the size of the LinkedList*/
+int ll_size(Node head);
--- src/linked-list/linkedlist.h
+++ src/linked-list/linkedlist.h
@@ -1,20 +1,158 @@
-#pragma  once
+#include "LinkedListImpl.h"
 
-#include <malloc.h>
+int ll_test_int() {
+    printf("test for int\n");
+    int x1 = 1000;
+    int x2 = 2000;
+    int x3 = 3000;
+    int x4 = 4000;
 
-typedef struct Node _Node;
+    void *px1 = &x1;
+    void *px2 = &x2;
+    void *px3 = &x3;
+    void *px4 = &x4;
 
-extern void print_all_linkedlist(_Node *head);
+    Node head_strings = ll_init(px1);
+    ll_add(head_strings, px2);
+    ll_add(head_strings, px3);
+    ll_add(head_strings, px4);
+    printf("before delete: \n");
+    printf("size: %d\n", ll_size(head_strings));
+    ll_print_all_type(head_strings, 'd');
+    ll_remove(&head_strings, px2);
+    printf("after delete: \n");
+    ll_print_all_type(head_strings, 'd');
+}
 
-extern void add_linkedlist(struct Node** head,int new_value);
+int ll_test_double() {
+    printf("test for double\n");
+    double x1 = 1000.25;
+    double x2 = 2000.55;
+    double x3 = 3000.45;
+    double x4 = 4000.55;
 
-extern int search_by_value_linkedlist(_Node *head, int value);
+    void *px1 = &x1;
+    void *px2 = &x2;
+    void *px3 = &x3;
+    void *px4 = &x4;
 
-extern void delete_by_value_linkedlist(struct Node **head_ref, int key);
+    Node head_doubles = ll_init(px1);
+    ll_add(head_doubles, px2);
+    ll_add(head_doubles, px3);
+    ll_add(head_doubles, px4);
+    printf("before delete: \n");
+    printf("size: %d\n", ll_size(head_doubles));
+    ll_print_all_type(head_doubles, 'f');
+    ll_remove(&head_doubles, px2);
+    printf("after delete: \n");
+    ll_print_all_type(head_doubles, 'f');
+}
 
-extern void private_swap_linkedlist(_Node *node1, _Node *node2);
 
-extern void sort_elements_linkedlist(_Node* head);
+int ll_test_strings() {
+    printf("test for strings\n");
+    char* x1 = {"Hello world"};
+    char* x2 = {"How are you"};
+    char* x3 = {"Nikos Koukos"};
+    char* x4 = {"John Smith"};
 
-extern int get_size_linkedlist(_Node* head);
+    Node head_strings = ll_init(x1);
+    ll_add(head_strings, x2);
+    ll_add(head_strings, x3);
+    ll_add(head_strings, x4);
+    printf("before delete: \n");
+    printf("size: %d\n", ll_size(head_strings));
+    ll_print_all_type(head_strings, 's');
+    ll_remove(&head_strings, x1);
+    printf("after delete: \n");
+    ll_print_all_type(head_strings, 's');
+}
+
+
+void LinkedList() {
+    ll_test_int();
+    ll_test_double();
+    ll_test_strings();
+}
+
+Node ll_init(void *value) {
+    Node head = malloc(sizeof(struct _Node));
+    head->value = value;
+    head->next = NULL;
+    return head;
+}
+
+void ll_add(Node head, void *value) {
+    Node current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    Node new_node = malloc(sizeof(struct _Node));
+    new_node->value = value;
+    new_node->next = NULL;
+    current->next = new_node;
+}
+
+void ll_remove(Node *head, void *value) {
+    Node current = *head;
+    Node prev = NULL;
+    while (current != NULL) {
+        if (current->value == value) {
+            if (prev == NULL) {
+                *head = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            free(current);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+void ll_print_all_type(Node head, char type) {
+    Node current = head;
+    int wrongType = 0;
+    while (current != NULL) {
+        switch (type) {
+            case 'd':
+                printf("%d \n", *(int *) current->value);
+                break;
+            case 'c':
+                printf("%c \n", *(char *) current->value);
+                break;
+            case 'f':
+                printf("%.2f \n", *(double *) current->value);
+                break;
+            case 's':
+                printf("%s \n", current->value);
+                break;
+            case 'p':
+                printf("%p \n", (void *) current->value);
+                break;
+            default:
+                printf("Please put a valid option: d,c,f,p \n");
+                wrongType = 1;
+                break;
+        }
+        if (wrongType == 0) {
+            current = current->next;
+        } else {
+            current = NULL;
+        }
+
+    }
+}
+
+int ll_size(Node head) {
+    int size = 0;
+    Node current = head;
+    while (current != NULL) {
+        size++;
+        current = current->next;
+    }
+    return size;
+}
+
 
