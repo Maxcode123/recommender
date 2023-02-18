7600f2243cc2776b286dfc841285e80fcf264a6d
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-02-18 11:40:10 +0200

    vector header

--- src/utils/vector.h
+++ src/utils/vector.h
@@ -7,5 +7,15 @@ typedef struct _Vector {
 
 Vector vct_init(int, int*);
 
+Vector vct_zeros(int);
+
+Vector vct_ones(int);
+
 void vct_add(Vector, Vector, Vector);
+
+void vct_subt(Vector, Vector, Vector);
+
+void vct_scale(Vector, double);
+
+double vct_norm(Vector);
 
