4ab5ba9c7770a6622e57bd1942ffdad5da0ac402
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-03-15 21:09:12 +0200

    vector test add

--- test/testvector.c
+++ test/testvector.c
@@ -11,3 +11,16 @@ Test(testvector, testscale) {
     cr_assert(vector_get(R, 1) == 2);
     cr_assert(vector_get(R, 2) == 3);
 }
+
+Test(testvector, testadd) {
+    int r1[] = {2, 4, 6};
+    Vector R1 = vector_init_by_array(3, r1);
+
+    int r2[] = {0, 14, 9};
+    Vector R2 = vector_init_by_array(3, r2);
+
+    Vector R = vector_add(R1, R2);
+    cr_assert(vector_get(R, 0) == 2, "%d != 2", vector_get(R, 0));
+    cr_assert(vector_get(R, 1) == 18, "%d != 28", vector_get(R, 1));
+    cr_assert(vector_get(R, 2) == 15, "%d != 15", vector_get(R, 2));       
+}
