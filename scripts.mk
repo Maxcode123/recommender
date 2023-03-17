0661e047569c9a91eee833ebc4c23752921e4c48
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-03-17 08:26:55 +0200

    use ht iterator in filter module

--- scripts.mk
+++ scripts.mk
@@ -1,28 +1,28 @@
-unique: $(OBJ)/unique.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/map.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
+unique: $(OBJ)/unique.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
 	$(CC) $^ -o $@
 	clear
 	./$@
 
-unique-filtered: $(OBJ)/unique_filtered.o $(OBJ)/filter.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/map.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
+unique-filtered: $(OBJ)/unique_filtered.o $(OBJ)/filter.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
 	$(CC) $^ -o $@
 	clear
 	./$@
 
-ratings: $(OBJ)/ratings.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/map.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
+ratings: $(OBJ)/ratings.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
 	$(CC) $^ -o $@
 	clear
 	./$@ 0 2 5 10 15
 	gnuplot plot/ratings_per_user_histogram.txt
 	code plot/ratings_per_user.jpg
 
-ratings-filtered: $(OBJ)/ratings_filtered.o $(OBJ)/filter.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/map.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
+ratings-filtered: $(OBJ)/ratings_filtered.o $(OBJ)/filter.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
 	$(CC) $^ -o $@
 	clear
 	./$@
 	gnuplot plot/ratings_per_user_histogram.txt
 	code plot/ratings_per_user.jpg
 
-dates: $(OBJ)/dates_filtered.o $(OBJ)/filter.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/map.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
+dates: $(OBJ)/dates_filtered.o $(OBJ)/filter.o $(OBJ)/histogram.o $(OBJ)/parser.o $(OBJ)/list.o $(OBJ)/hashset.o $(OBJ)/date.o $(OBJ)/lex.yy.o
 	$(CC) $^ -o $@
 	clear
 	./$@ 0 365 730 1095
--- src/preprocess/filter.c
+++ src/preprocess/filter.c
@@ -4,12 +4,13 @@ List filter(HashTable usermap, List rlst) {
     List lst;
     int len;
     List fltr = lst_list();
-    for (int i = 0; i < ht_size(usermap); i++) {
-        lst = (List)(usermap->entries[i].value);
+    Iterator it = ht_it(usermap);
+    while (ht_next(it)) {
+        lst = (List)(it->value);
         len = lst_len(lst);
         if (len == 0) continue;
         if (len < RMIN || len > RMAX) {
-            ht_delete(usermap, usermap->entries[i].key);
+            ht_delete(usermap, it->key);
             continue;
         }
         while (lst != NULL) {
