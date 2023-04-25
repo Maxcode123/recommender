72282d42de88467554dbca7d796ae565422b534a
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-04-25 21:06:00 +0300

    main

--- test/main.c
+++ test/main.c
@@ -9,74 +9,22 @@
 
 
 int main(int argc, char **argv) {
-    int n[] = {9, 3, 3};
-    NeuralNetwork NN = neuralnet(3, n);
-    netinit(NN);
+    int k = 2;
+    int n = 6;
+    int d = 4;
+    Vector *R = malloc(sizeof(*R) * n);
+    double r1[] = {0, 0, 5, 9};
+    double r2[] = {0, 0, 2, 5};
+    double r3[] = {0, 0, 9, 9};
+    double r4[] = {5, 4, 0, 0};
+    double r5[] = {1, 1, 0, 0};
+    double r6[] = {5, 1, 0, 0};
+    R[0] = vector_init_by_array(d, r1);
+    R[1] = vector_init_by_array(d, r2);
+    R[2] = vector_init_by_array(d, r3);
+    R[3] = vector_init_by_array(d, r4);
+    R[4] = vector_init_by_array(d, r5);
+    R[5] = vector_init_by_array(d, r6);
 
-    netprint(NN);
-
-    Matrix X = matrix(3, 9);
-    
-    X->matrix[0][0] = 6;
-    X->matrix[0][1] = 9;
-    X->matrix[0][2] = 5.5;
-    X->matrix[0][3] = 7.2;
-    X->matrix[0][4] = 8.4;
-    X->matrix[0][5] = 4;
-    X->matrix[0][6] = 6.5;
-    X->matrix[0][7] = 9.1;
-    X->matrix[0][8] = 6.5;
-
-    X->matrix[1][0] = 7;
-    X->matrix[1][1] = 8;
-    X->matrix[1][2] = 9.3;
-    X->matrix[1][3] = 6.5;
-    X->matrix[1][4] = 7.1;
-    X->matrix[1][5] = 9.8;
-    X->matrix[1][6] = 5;
-    X->matrix[1][7] = 7.7;
-    X->matrix[1][8] = 8.4;
-
-    X->matrix[2][0] = 3;
-    X->matrix[2][1] = 4;
-    X->matrix[2][2] = 4.3;
-    X->matrix[2][3] = 3.5;
-    X->matrix[2][4] = 3.9;
-    X->matrix[2][5] = 4.5;
-    X->matrix[2][6] = 3.2;
-    X->matrix[2][7] = 4;
-    X->matrix[2][8] = 4;
-
-    Matrix Y = matrix(3, 3);
-    
-    Y->matrix[0][0] = 6.4;
-    Y->matrix[0][1] = 8.6;
-    Y->matrix[0][2] = 5.5;
-    Y->matrix[1][0] = 7;
-    Y->matrix[1][1] = 8;
-    Y->matrix[1][2] = 9;
-    Y->matrix[2][0] = 3;
-    Y->matrix[2][1] = 4;
-    Y->matrix[2][2] = 3; 
-
-
-    train(NN, X, Y, 0.0001, 10000);
-
-    netprint(NN);
-
-    Vector x = vector_create(9);
-    vector_push(x, 6);
-    vector_push(x, 9);
-    vector_push(x, 5.5);
-    vector_push(x, 7.2);
-    vector_push(x, 8.4);
-    vector_push(x, 4);
-    vector_push(x, 6.5);
-    vector_push(x, 9.1);
-    vector_push(x, 6.5);
-
-    
-    Vector y_ = forward(NN, x);
-
-    vector_print(y_);
+    clustering(R, k, n, 100, &calcd_perc);
 }
